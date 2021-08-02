ef26ca4d309b7ec1bfa4739395656f8694754fdf
XIA Jiazhen <xiajiazhen@gmail.com>
2021-08-03 00:31:48 +0800

    subspace_mul_kernel_diagonal_4x4!

--- src/mul/kernel/diagonal.jl
+++ src/mul/kernel/diagonal.jl
@@ -11,21 +11,11 @@ indices: array of indices from comspace
 end
 
 @inline function subspace_mul_kernel_diagonal_4x4!(S::AbstractVector{T}, indices, U, k::Int, offset::Int) where T
-    # TODO
-    #
-    # @inbounds begin
-    #     Base.Cartesian.@nexprs 4 i -> begin
-    #         y[i] = zero(T)
-    #         Base.Cartesian.@nexprs 4 j -> begin
-    #             idx_j = k + indices[j] + offset
-    #             y[i] += U[i, j] * S[idx_j]
-    #         end
-    #     end
-
-    #     Base.Cartesian.@nexprs 4 i -> begin
-    #         idx_i = k + indices[i] + offset
-    #         S[idx_i] = y[i]
-    #     end
-    # end
-    # return
+    @inbounds begin
+        Base.Cartesian.@nexprs 4 i -> begin
+            idx_i = k + indices[i] + offset
+            S[idx_i] = U[i, i] * S[idx_i]
+        end
+    end
+    return
 end
