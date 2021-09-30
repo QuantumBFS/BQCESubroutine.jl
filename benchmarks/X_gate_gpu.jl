61dd44badd3572fec1c08a31859cae464718219d
XIA Jiazhen <xiajiazhen@gmail.com>
2021-10-01 00:17:48 +0800

    fix CUDA starting code

--- benchmarks/X_gate_gpu.jl
+++ benchmarks/X_gate_gpu.jl
@@ -6,9 +6,10 @@ using YaoLocations
 using BQCESubroutine: threaded_basic_broutine!
 
 N = 5
+loc = 1
 X = [0 1; 1 0]
-locs = Locations(1)
+locs = Locations(loc)
 st = rand(Float64, 1<<N);
-st0 = broutine!(copy(st), Val(:X_test_gpu), locs);
+st0 = BQCESubroutine.subspace_mul!(copy(st), Val(:X_test_gpu), loc);
 st1 = broutine!(copy(st), [0 1; 1 0], locs);
 st0 ≈ st1
