fa9a9f51c1637e679b2ba0d14d28d36ba6373ce0
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2022-02-23 12:00:30 +0800

    benchmark generic

--- benchmarks/generic_threads.jl
+++ benchmarks/generic_threads.jl
@@ -10,14 +10,15 @@ locs = Locations(shift+1:shift+M)
 subspace = BQCESubroutine.bsubspace(N, locs)
 comspace = BQCESubroutine.bcomspace(N, locs)
 st = rand(T, 1<<N); U = rand(Float64, 1<<M, 1<<M);
+BQCESubroutine.threaded_subspace_mul_generic!(st, comspace, U, subspace)
 
 nthreads = Threads.nthreads()
-print("nthreads=$(nthreads), N=$N, M=$M")
-
+println("nthreads=$(nthreads), N=$N, M=$M, shift=$shift")
 if nthreads == 1
     BQCESubroutine.disable_threads()
-    @benchmark BQCESubroutine.subspace_mul!(st, comspace, U, subspace) setup=(st = rand(T, 1<<N); U = rand(T, 1<<M, 1<<M))
+    t = @benchmark BQCESubroutine.subspace_mul!(st, comspace, U, subspace) setup=(st = rand(T, 1<<N); U = rand(T, 1<<M, 1<<M))
 else
     BQCESubroutine.enable_threads()
     @benchmark BQCESubroutine.threaded_subspace_mul_generic!(st, comspace, U, subspace) setup=(st = rand(T, 1<<N); U = rand(T, 1<<M, 1<<M))
 end
+println("time = $(mean(t).time/1e6) ms")
--- benchmarks/generic_threads.sh
+++ benchmarks/generic_threads.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..6}
+for i in {1..8}
 do
    JULIA_NUM_THREADS=$i julia --project=@. generic_threads.jl
 done
